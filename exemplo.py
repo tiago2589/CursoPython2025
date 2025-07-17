37f7905b0c1adc2f4bb60eae0704443b4c57fb5d
Tiago <tiago2589@yahoo.com.br>
2025-07-17 12:37:31 -0300

    adicionando informacoes git

--- exemplo.py
+++ exemplo.py
@@ -11,3 +11,15 @@ print("Seu nome é: ", nome, "e sua idade é:", idade)
 
 curso = "Python desenvolvedor"
 print("Curso:", curso)
+
+#git config --global user.name "Tiago"
+#git config --global user.email """
+# git init - inicializa o repositório   
+#git log - ver o que foi mudado
+#git status - verificar o que foi modificado e como está o repositório
+#git branch - ver as branches
+#git add exemplo.py - adiciona o arquivo ao stage
+#git add . - adiciona todos os arquivos ao stage
+#git commit -m "Adicionando exemplo.py", -m significa mensagem
+#git push -u origin main - envia para o repositório remoto
+#git pull - puxa as alterações do repositório remoto    
