24471998224c4275280e89a3c849d84cff89916e
Tiago <tiago2589@yahoo.com.br>
2025-07-17 12:15:19 -0300

    Teste Curso Pyhton 2025

---
+++ exemplo.py
@@ -0,0 +1,10 @@
+print("Hello World")
+idade = 25
+print(idade)
+print("Testando Pyhton")
+
+nome = input("digite seu nome:")
+print (nome)
+
+idade = int(input("Digite sua idade: "))
+print("Seu nome é: ", nome, "e sua idade é:", idade)
